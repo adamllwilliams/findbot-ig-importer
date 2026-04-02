9bd6fff825bf1c85ac8a25d93864c30581961e89
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-04-02 14:11:58 +0100

    add query param

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@ exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v4! Your function executed successfully!",
+      url: event.queryStringParameters?.url,
     }),
   };
 };
