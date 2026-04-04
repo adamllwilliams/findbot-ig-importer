17e365e0b76534d28dde2a8b0669548ffc2fdaed
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-04-04 12:13:35 +0100

    scrape using apify

--- src/handler.ts
+++ src/handler.ts
@@ -1,28 +1,36 @@
 import { ApifyClient } from 'apify-client';
 
 exports.hello = async (event) => {
-  const client = new ApifyClient({
-    token: process.env.APIFY_TOKEN,
-  });
+  // SCRAPE
+  const res = await fetch(
+    `https://api.apify.com/v2/acts/apify~instagram-post-scraper/run-sync-get-dataset-items?token=${process.env.APIFY_TOKEN}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: [event.queryStringParameters?.url],
+        resultsLimit: 24,
+        dataDetailLevel: 'basicData'
+      })
+    }
+  );
 
-  const input = {
-    "username": [
-        "https://www.instagram.com/p/DVJudAwiLW3/",
-    ],
-    "resultsLimit": 24,
-    "dataDetailLevel": "basicData"
-  };
+  const items = await res.json();
+  const displayUrl = items[0].displayUrl;
+  const caption = items[0].caption;
 
-  const run = await client.actor("apify/instagram-post-scraper").call(input);
-
-  const { items } = await client.dataset(run.defaultDatasetId).listItems();
+  const imageRes = await fetch(displayUrl);
+  const buffer = await imageRes.arrayBuffer();
+  const base64 = Buffer.from(buffer).toString('base64');
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v4! Your function executed successfully!",
-      displayUrl: items[0].displayUrl,
-      caption: items[0].caption,
+      displayUrl: displayUrl,
+      caption: caption,
     }),
   };
 };
