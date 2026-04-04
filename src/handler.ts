6ab87502c2535a140601d153fb3b18841e4b715c
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-04-04 11:18:15 +0100

    use apify

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,28 @@
+import { ApifyClient } from 'apify-client';
+
 exports.hello = async (event) => {
+  const client = new ApifyClient({
+    token: process.env.APIFY_TOKEN,
+  });
+
+  const input = {
+    "username": [
+        "https://www.instagram.com/p/DVJudAwiLW3/",
+    ],
+    "resultsLimit": 24,
+    "dataDetailLevel": "basicData"
+  };
+
+  const run = await client.actor("apify/instagram-post-scraper").call(input);
+
+  const { items } = await client.dataset(run.defaultDatasetId).listItems();
+
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v4! Your function executed successfully!",
-      igUrl: event.queryStringParameters?.url,
+      displayUrl: items[0].displayUrl,
+      caption: items[0].caption,
     }),
   };
 };
