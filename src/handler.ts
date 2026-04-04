179ac81ea650fcecdb6f0ddd0eeef192f2138b07
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-04-04 13:18:38 +0100

    simplify response body

--- src/handler.ts
+++ src/handler.ts
@@ -77,11 +77,6 @@ exports.hello = async (event) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-      displayUrl: displayUrl,
-      caption: caption,
-      eventData: eventData,
-    }),
+    body: JSON.stringify(eventData),
   };
 };
